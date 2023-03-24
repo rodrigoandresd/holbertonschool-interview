07b36891677a582fb7fe6920d28262c1d9dbd928
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-24 18:26:00 -0500

    tringle pascal

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    """
+    Returns the Pascal's triangle of size n as a list of lists.
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = prev_row[j-1] + prev_row[j]
+        triangle.append(row)
+    return triangle
