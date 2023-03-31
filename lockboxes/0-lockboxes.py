c7e1c6abc9e0b9cba4ee17dedafbf53c7164c29f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-30 22:28:13 -0500

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,7 +7,7 @@ and each box may contain keys to the other boxes.
 
 
 def canUnlockAll(boxes):
-    
+
     n = len(boxes)
     visited = [False] * n  # initially, no boxes are visited
     visited[0] = True  # mark the first box as visited
@@ -15,7 +15,9 @@ def canUnlockAll(boxes):
     while queue:
         box = queue.pop(0)  # get the next box from the queue
         for key in boxes[box]:  # check all keys in the box
-            if key < n and not visited[key]:  # make sure the key is valid and not visited yet
+            if key < n and not visited[key]:
+            # make sure the key is valid and not visit
                 visited[key] = True  # mark the box as visited
-                queue.append(key)  # add the box to the queue for further exploration
+                queue.append(key)
+                # add the box to the queue for further exploration
     return all(visited)  # check if all boxes are visited
