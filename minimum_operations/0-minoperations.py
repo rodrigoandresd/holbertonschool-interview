9c9274c79ddc7525c295d76844515673142657ac
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-04-07 19:40:18 -0500

    minoperations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,37 +5,18 @@ to result in exactly n H characters in the file.
 """
 
 
-def copyAll(myList):
-    """method that copy a str"""
-    myList[0] = myList[0] + 1
-    myList[2] = myList[1]
-    # print("copy", myList)
-    return (myList)
-
-
-def paste(myList):
-    """method that paste a str"""
-    myList[0] = myList[0] + 1
-    myList[1] = myList[1] + myList[2]
-    # print("paste", myList)
-    return (myList)
-
-
 def minOperations(n):
     """method that calculates the fewest number of operations needed"""
-    chars = "H"
-    string = chars
-    count = 0
-    myData = [count, string, chars]
+    if n <= 1 or type(n) is not int:
+        return 0
 
-    if type(n) is not int:
-        return (0)
-    if n <= 1:
-        return (0)
-    while len(myData[1]) < n:
-        if n % len(myData[1]) == 0:
-            myData = copyAll(myData)
-            myData = paste(myData)
+    count = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            count += i
+            n /= i
         else:
-            myData = paste(myData)
-    return myData[0]
+            i += 1
+
+    return count
