ad68a14533fc4858608318aa66dcbae813bf00bc
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-05-07 15:28:10 -0500

    starwars

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,25 +1,45 @@
 #!/usr/bin/node
-const { get } = require('request');
-const id = process.argv[2];
-const baseUrl = `https://swapi-api.hbtn.io/api/films/${id}`;
+/*
+    Write a script that prints all characters of a Star Wars movie:
+    The first positional argument passed is the Movie ID -
+    example: 3 = “Return of the Jedi”
+    Display one character name per line in the same order
+    as the “characters” list in the /films/ endpoint
+    You must use the Star wars API
+    You must use the request module
+*/
 
-const getCharacter = (character) => new Promise((resolve, reject) => {
-  get(character, (err, data) => {
-    if (err) {
-      reject(err);
-      return;
+const args = process.argv.slice(2);
+if (args < 1) {
+  console.log('Error');
+  process.exit(1);
+}
+
+const episode = args[0];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
+const request = require('request');
+let characters = [];
+request(url, (err, resp, body) => {
+  if (err || resp.statusCode !== 200) console.log(err);
+  else characters = JSON.parse(body).characters;
+  const size = Object.keys(characters).length;
+  const array = Array(size).fill();
+  let data = 0;
+  for (let i = 0; i < size; i++) {
+    request(characters[i], (erro, respo, bodys) => {
+      if (erro || respo.statusCode !== 200) console.log(erro);
+      else {
+        array[i] = JSON.parse(bodys).name;
+        data++;
+      }
+      if (data === size) {
+        for (let j = 0; j < size; j++) {
+          console.log(array[j]);
+        }
+      }
     }
 
-    const { name } = JSON.parse(data.body);
-    resolve(name);
-  });
-});
-
-get(baseUrl, (err, data) => {
-  if (err) console.error(err);
-  const { characters } = JSON.parse(data.body);
-
-  const promises = characters.map(character => getCharacter(character));
-
-  Promise.all(promises).then(names => names.forEach(name => console.log(name)));
+    );
+  }
 });
